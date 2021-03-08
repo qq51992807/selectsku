019783dd3864922ee740d38d9dde40904afe6692
yang <51992807@qq.com>
2021-03-08 21:52:55 +0800

    add

--- src/main/java/com/yang/selectsku/DyPost/autoBuyDy.java
+++ src/main/java/com/yang/selectsku/DyPost/autoBuyDy.java
@@ -28,7 +28,7 @@ public class autoBuyDy implements Runnable{
     //        String authorId="66916631931";//99514375927
 //        String productId="3466033593078445180";
     // List comboIds=dyGetCombIdsUtils.getCombIds("3466425772766549575");
-    public static void main(String args[]){
+     public static void main(String args[]){
 //        autoBuyDy autoBuyDy=new autoBuyDy("鞋子","70204787595","AQ1316003");
 //        autoBuyDy autoBuyDy=new autoBuyDy("黄金","2726434986791752","吉祥如意圆形足金");//黄金
 //        new Thread(autoBuyDy,"auto").start();
@@ -39,16 +39,15 @@ public class autoBuyDy implements Runnable{
         for(int i=0;i<5 ;i++){   int num=i+1;
             List<String> list=new ArrayList<>();
             list.add("1693113852524589");
-            //NK女运动鞋生活系列DD1503102#HLJ   3466571539711943980 价格699 已售0
-            new Thread(new commitDy(itemName,authorId,"3466571539711943980",dyGetCombIdsUtils.getCombIds("3466571539711943980"),false),"抢购线程"+num).start();
-
+//            new Thread(new commitDy(itemName,authorId,"3457480459737508175",dyGetCombIdsUtils.getCombIds("3457480459737508175"),false),"抢购线程"+num).start();
+//            new Thread(new commitDy(itemName,authorId,"3466571539711943980",dyGetCombIdsUtils.getCombIds("3466571539711943980"),false),"抢购线程"+num).start();
 
             //测试用
 //          new Thread(new commitDy(itemName,authorId,"3466425291780526529",dyGetCombIdsUtils.getCombIds("3466425291780526529"),false),"抢购线程"+num).start();
        }
-        for(int i=0;i<1;i++){
-            new Thread(new autoBuyDy("蓝","70204787595","蓝"),"auto").start();
-        }
+//        for(int i=0;i<1;i++){
+//            new Thread(new autoBuyDy("蓝","70204787595","蓝"),"auto").start();
+//        }
     }
 
     @Override
--- src/main/java/com/yang/selectsku/utils/dyProductSearchUtils.java
+++ src/main/java/com/yang/selectsku/utils/dyProductSearchUtils.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class dyProductSearchUtils {
     public static void main(String args[]){
-        dyProductSearchUtils.searchProduct("70204787595","");
+//        dyProductSearchUtils.searchProduct("70204787595","");
 //        dyProductSearchUtils.searchProduct("75317185876","");//英伟达
 //        dyProductSearchUtils.searchProduct("4195355415549012","");//罗永浩
 //        dyProductSearchUtils.searchProduct("66916631931","");//罗永浩
@@ -27,9 +27,10 @@ public class dyProductSearchUtils {
 //        dyProductSearchUtils.searchProduct("94507208961","");//心跳百货
 //        dyProductSearchUtils.searchProduct("272306652324237","");//微型  4441647751118387  恰自来   272306652324237
 //        dyProductSearchUtils.getShopList("HInDTtqO","");//微型
-//        dyProductSearchUtils.getShopList("sHXbwMK","");//胜道sHXbwMK
+        dyProductSearchUtils.getShopList("sHXbwMK","");//胜道sHXbwMK
 //        dyProductSearchUtils.getShopList("RROoNoaL","");//工蜂研机社
 //        dyProductSearchUtils.getShopList("svzTOXE","");//铭瑄官方
+//        dyProductSearchUtils.getShopList("CiTMufNE","");//rog国度
 
     } //70204787595 胜道 //68208641192 c
 
@@ -65,7 +66,7 @@ public class dyProductSearchUtils {
         String productId = null;
         for (int index = 0; index < 50; index++) {
             try {//https://api5-normal-lite-act-lq.amemv.com/aweme/v1/promotion/user/promotion/list/?aid=2329&cursor=0&sort=0&count=1&user_id=70204787595&column_id=1&goods_type=1
-                String json = getUtils.httpGet("https://ec.snssdk.com/productcategory/getShopList?shop_id=" + authorId + "&type=8&sort=0&size=100&page=" + index + "&b_type_new=0&device_id=0&is_outside=1", "UTF-8");
+                String json = getUtils.httpGet("https://ec.snssdk.com/productcategory/getShopList?shop_id=" + authorId + "&type=8&sort=0&size=100&page="+index+"&b_type_new=0&device_id=0&is_outside=1", "UTF-8");
                 JSONObject jsonObject = new JSONObject(json);
                 JSONObject data = new JSONObject(jsonObject.getString("data"));
                 JSONArray jsonArray = new JSONArray(data.getString("list"));
